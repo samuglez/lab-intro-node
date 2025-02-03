87b5034ac15dfc74a1f3dd3b27d1fbcf1859ea15
unknown <samuglez90@gmail.com>
2025-02-03 13:17:09 +0100

    Hecho Samuel

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    
+      this.items = [],
+      this.length = this.items.length
+    
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.items.sort((a,b)=> a-b)
+    this.length = this.items.length
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos > this.length) {
+     throw new Error("OutOfBounds");
+    }
+    if(pos < this.length){
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return this.items[this.items.length-1]
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error("Empty SortedList");
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+   return this.items.reduce((acc, curre)=> acc + curre , 0) 
+  }
 
-  avg() {}
+  avg() {
+    if(this.length === 0){
+      throw new Error("Empty SortedList")
+    }
+    return this.sum()/ this.items.length
+  }
 }
 
 module.exports = SortedList;
